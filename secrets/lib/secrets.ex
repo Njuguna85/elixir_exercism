1503fce7a75680e511ed4fd1430cec6769140d60
Dennis Njuguna <dennix.njuguna85@gmail.com>
2023-04-14 13:58:17 +0300

    secrets

---
+++ secrets/lib/secrets.ex
@@ -0,0 +1,30 @@
+defmodule Secrets do
+  def secret_add(secret) do
+    # Please implement the secret_add/1 function
+    fn arg -> secret + arg end
+  end
+
+  def secret_subtract(secret) do
+    # Please implement the secret_subtract/1 function
+    fn arg -> arg - secret end
+  end
+
+  def secret_multiply(secret), do: &(&1 * secret)
+
+  def secret_divide(secret), do: &(div(&1, secret)) |> trunc()
+
+  def secret_and(secret) do
+    # Please implement the secret_and/1 function
+    fn arg -> Bitwise.band(arg, secret) |> trunc() end
+  end
+
+  def secret_xor(secret) do
+    # Please implement the secret_xor/1 function
+    fn arg -> Bitwise.bxor(arg, secret) |> trunc() end
+  end
+
+  def secret_combine(secret_function1, secret_function2) do
+    # Please implement the secret_combine/2 function
+    fn x -> x |> secret_function1.() |> secret_function2.() end
+  end
+end
