7cc4c6be8b4ed3f9b8ce4e1d36a671acbeb9fc8a
Dennis Njuguna <dennix.njuguna85@gmail.com>
2023-04-17 17:20:52 +0300

    guessing game

---
+++ guessing-game/lib/guessing_game.ex
@@ -0,0 +1,15 @@
+defmodule GuessingGame do
+  def compare(secret_number, guess) when is_atom(guess), do: "Make a guess"
+
+  def compare(secret_number, guess) when secret_number == guess, do: "Correct"
+
+  def compare(secret_number, guess) when abs(secret_number - guess) == 1, do: "So close"
+
+  def compare(secret_number, guess) when guess > secret_number, do: "Too high"
+
+  def compare(secret_number, guess) when guess < secret_number, do: "Too low"
+
+  def compare(_), do: "Make a guess"
+
+
+end
